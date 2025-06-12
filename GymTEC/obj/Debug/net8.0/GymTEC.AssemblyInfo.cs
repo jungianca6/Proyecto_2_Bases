239c6568a3334853081f1f0000bc0fcb1881116f
dargarro@estudiantec.cr <dargarro@estudiantec.cr>
2025-06-12 00:02:44 -0600

    Base de datos

--- GymTEC/obj/Debug/net8.0/GymTEC.AssemblyInfo.cs
+++ GymTEC/obj/Debug/net8.0/GymTEC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymTEC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb8b6ea13ae72b09c643380b76e9410dd1b1970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7f9005b543da149334212565c738c7faffd805f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymTEC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymTEC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
