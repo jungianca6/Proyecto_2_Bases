e8565ed8be1e2d7bf9ef729deaf06a0f87c0ab12
Brayan Alpizar <brayanalpizar36@gmail.com>
2025-06-18 01:25:38 -0600

    Se agregaron los tipos de planilla

--- GymTEC/Controllers/PayrollController.cs
+++ GymTEC/Controllers/PayrollController.cs
@@ -49,5 +49,49 @@ namespace GymTEC.Controllers
 
             return Ok(response);
         }
+
+        [HttpPost("manage_payroll_type")]
+        public ActionResult<Data_response<string>> ManagePayrollType([FromBody] Data_input_manage_payroll_type input)
+        {
+            // Aquí la lógica para insertar o editar el tipo de planilla (simulación)
+            string msg = $"Tipo de planilla '{input.description}' con ID '{input.identifier}' guardado correctamente.";
+
+            return Ok(new Data_response<string>
+            {
+                status = true,
+                data = msg
+            });
+        }
+
+        [HttpPost("delete_payroll_type")]
+        public ActionResult<Data_response<string>> DeletePayrollType([FromBody] Data_input_delete_payroll_type input)
+        {
+            // Aquí iría la lógica real de eliminación en base al identificador
+
+            string msg = $"Tipo de planilla con ID '{input.identifier}' eliminado correctamente.";
+
+            return Ok(new Data_response<string>
+            {
+                status = true,
+                data = msg
+            });
+        }
+
+        [HttpPost("get_payroll_type")]
+        public ActionResult<Data_response<Data_output_get_payroll_type>> GetPayrollType([FromBody] Data_input_get_payroll_type input)
+        {
+            // Simulación de búsqueda (en la práctica, consultar en base de datos)
+            var result = new Data_output_get_payroll_type
+            {
+                identifier = input.identifier,
+                description = "Planilla de Entrenadores"
+            };
+
+            return Ok(new Data_response<Data_output_get_payroll_type>
+            {
+                status = true,
+                data = result
+            });
+        }
     }
 }
--- GymTEC/obj/Debug/net8.0/GymTEC.AssemblyInfo.cs
+++ GymTEC/obj/Debug/net8.0/GymTEC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymTEC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13d47e2a62bb6b9bc92551a17f0a56bd88fb127c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+363a59778f689bc9d1b8c6753622f3f791de2b62")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymTEC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymTEC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
